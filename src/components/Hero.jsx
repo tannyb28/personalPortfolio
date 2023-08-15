e01c40d3860eb6b5a0c853a91b6aed3ffccd3c29
tannyb28 <tanishbhowmick@gmail.com>
2023-08-15 00:38:55 -0500

    First commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,100 @@
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { Link } from "react-router-dom"
+import { ComputersCanvas } from "./canvas"
+import {Cursor, useTypewriter} from 'react-simple-typewriter'
+import {AiOutlineMail} from 'react-icons/ai'
+import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+
+
+
+const Hero = () => {
+  const [text, count] = useTypewriter({
+    words: [
+      `Biomedical Engineer`,
+      `Software Developer`,
+      `University of Texas at Austin`,
+      `Class of 2024`
+      
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  })
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#cc5500]" />
+          <div className="w-1 sm:h-80 h-40 orange-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#cc5500]">Tanish</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 h-[50px]`}>
+            {/* I develop a bunch of cool stuff. <br className="sm:block hidden" />
+            Check me out! */}
+            <span className="uppercase tracking-widest">{text}</span>
+          </p>
+
+          <div className='z-20 flex justify-between max-w-[280px] py-4'>
+            <div
+              onClick={()=>window.open("https://www.linkedin.com/in/tanish-bhowmick","_blank")} 
+              className='z-20 rounded-full bg-[#cc5500] shadow-md shadow-black p-3 cursor-pointer hover:scale-110 ease-in duration-200'
+            >
+              <FaLinkedinIn />
+            </div>
+
+            <div
+              onClick={()=>window.open("https://www.github.com/tannyb28","_blank")} 
+              className='z-20 rounded-full bg-[#cc5500] shadow-md shadow-black p-3 cursor-pointer hover:scale-110 ease-in duration-200'
+            >
+              <FaGithub />
+            </div>
+
+            <div
+              onClick={()=>{
+                const section = document.querySelector( '#contact' );
+                section.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+              }
+              } 
+              className='z-20 rounded-full bg-[#cc5500] shadow-md shadow-black p-3 cursor-pointer hover:scale-110 ease-in duration-200'
+            >
+              <AiOutlineMail />
+            </div>
+            
+            <div
+              onClick={()=>window.open("https://drive.google.com/file/d/116f2AeRVW_qczrjgvUG4QsUGn7XbUY0c/view?usp=sharing","_blank")} 
+              className='z-20 rounded-full bg-[#cc5500] shadow-md shadow-black p-3 cursor-pointer hover:scale-110 ease-in duration-200'
+            >
+              <BsFillPersonLinesFill />
+            </div>
+
+          </div>
+          
+        </div>
+      </div>
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/constants/index.js
@@ -0,0 +1,270 @@
+import {
+  afib,
+  alois,
+  ambulance,
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  geobyte,
+  verato,
+  research,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Biomedical Engineer",
+    icon: creator,
+  },
+  {
+    title: "Full Stack Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "ML/Data Engineer",
+    icon: web,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Data Science Intern",
+    company_name: "Verato",
+    icon: verato,
+    iconBg: "#eee",
+    date: "May 2023 - Present",
+    points: [
+      "Evaluating third-party APIs for geolocation data enrichment in a dataset of 400 million patients’ records, resulting in a 98% fill rate in geolocation data.",
+      "Architecting a scalable data processing system that reads and analyzes data from Snowflake, enabling geolocation tagging, proximity calculations using geohexing, and location matching at a rate of ∼700K records per second.",
+      "Integrating selected APIs into the existing data infrastructure, resulting in enhanced geospatial insights for patient data.",
+      "Compiling data queries from Snowflake queries and creating visualizations in a Python web app using Folium and Streamlit."
+    ],
+  },
+  {
+    title: "Undergraduate Research Assistant",
+    company_name: "Dynamic Medical Image and Computing Lab",
+    icon: research,
+    iconBg: "#eee",
+    date: "August 2022 - May 2023",
+    points: [
+      "Research lab focused on automating the process of image segmentation on CT-images of the lung as an alternative imaging modality to current lung tumor diagnosis modalities.",
+      "Manually segmented CT-images of the lungs to create training data for the image segmentation model using Matlab.",
+      "Used PyTorch and OpenCV to create a deep neural network that can identify where a lung tumor occurs based on segmented CT-scan training data.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: `Geobyte | geobyte.ai`,
+    icon: geobyte,
+    iconBg: "#000",
+    date: "May 2022 - May 2023",
+    points: [
+      "Developed iOS application that will capture odometry data from Apple device LiDAR scanners to create accurate 3D models of the user’s surroundings.",
+      "Generated image segmentation model with PyTorch that will be able to identify people, cars, etc. from the 3D models and remove them to create better 3D maps and promote personal privacy.",
+      "Created full-stack service that takes 3D model from AWS, decompresses the model using the Draco protocol, and renders the 3D model on an individual users homepage.",
+    ],
+  }
+  // },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Ambulance Tracker",
+    description:
+      "Mobile application that allows patients in rural India to search and connect with amulance drivers near them, providing an efficient solution for health transportation.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: ambulance,
+    source_code_link: "https://github.com/tannyb28/AmbuTrack",
+  },
+  {
+    name: "Afib Detection",
+    description:
+      "Deep learning model trained with Keras to classify ECG data as normal, atrial fibrillation, noisy, or other.",
+    tags: [
+      {
+        name: "python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "kerasapi",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: afib,
+    source_code_link: "https://github.com/tannyb28/afib",
+  },
+  {
+    name: "Alzheimer's Care App",
+    description:
+      "A social media platform designed to give caretakers of Alzheimer's patients a community and resource hub to properly take care of their loved ones.",
+    tags: [
+      {
+        name: "reactnative",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: alois,
+    // source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
